2d5162a2000e47e03dcf2c8c0c9b5cffa61a269e
maia <gu.mmeneses@gmail.com>
2020-01-16 17:54:22 -0300

    eslinted

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const express    = require ('express');
-const bodyParser = require('body-parser');
-const CryptoJS = require("crypto-js");
-const app        = express();
-const port       = 3000;
-const db         = require('./models/queries.js');
-const usr        = require('./models/users.js');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const db = require('./models/queries.js')
+const usr = require('./models/users.js')
 
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
-	  extended: true,
-	 })
+    extended: true
+  })
 
 )
 
-
 app.get('/', (req, res) => {
-  res.json({ info: 'Node.js, Express, and Postgres API. This is the core service of Tinoite.'})
+  res.json({ info: 'Node.js, Express, and Postgres API. This is the core service of Tinoite.' })
 })
 
 app.get('/users', usr.getUsers)
 
 app.get('/users/:id', usr.getUserById)
 
-app.post('/users', usr.createUser);
+app.post('/users', usr.createUser)
 
-app.post('/gender', db.createGender);
+app.post('/gender', db.createGender)
 
 app.put('/users/:id', usr.updateUser)
 
@@ -33,4 +31,4 @@ app.delete('/users/:id', usr.deleteUser)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
- })
+})
--- models/queries.js
+++ models/queries.js
@@ -1,32 +1,27 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-	user: 'me',
-	host: 'localhost',
-	database: 'tnt_core',
-	password: 'password',
-	port:     '5432'
+  user: 'me',
+  host: 'localhost',
+  database: 'tnt_core',
+  password: 'password',
+  port: '5432'
 })
-
-
 
 const createGender = (req, res) => {
-
   const {
-		gender
-	, sta_active
-} = req.body
+    gender
+    , staActive
+  } = req.body
 
-pool.query('INSERT INTO tnt_genders (gender, sta_active) VALUES ($1, $2)', [gender, sta_active], (error, results) => {
-	  if(error) {
-			throw error
-		}
+  pool.query('INSERT INTO tnt_genders (gender, sta_active) VALUES ($1, $2)', [gender, staActive], (error, results) => {
+    if (error) {
+      throw error
+    }
 
-		res.status(200).send(`Gender inserted with the id: ${results.insertId} `)
-})
+    res.status(200).send(`Gender inserted with the id: ${results.insertId} `)
+  })
 }
 
-
-
 module.exports = {
- createGender
+  createGender
 }
