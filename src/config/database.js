baf45d864291fcc255009a2f36008fed6cc45a7c
maia <gu.mmeneses@gmail.com>
2020-01-18 15:16:28 -0300

    core app working

--- src/config/database.js
+++ src/config/database.js
@@ -8,4 +8,5 @@ module.exports = {
     timestamps: true,
     underscored: true,
   },
+    logging: false
 };
--- src/controllers/LikesController.js
+++ src/controllers/LikesController.js
@@ -1,12 +1,8 @@
 const Like = require('../models/Likes')
 const User = require('../models/Users')
 
-function isLikedBy(likedUser, Liker){
-
-}
 
 module.exports = {
-
 
  async likeUser(req, res) {
 
@@ -15,9 +11,21 @@ module.exports = {
  const matcher = await User.findByPk(matcher_id);
  const matched = await User.findByPk(matched_id);
 
+ const isMatched = await Like.findAll({
+    where: {
+      matcher_id: matched_id,
+      matched_id: matcher_id,
+      },
+    })
+
  const like = await Like.create({ matcher_id, matched_id, sta_like });
 
- console.log(`${matcher.name} (${matcher.id}) Liked ${matched.name} (${matched.id}).`)
+ console.log(`${matcher.name} (${matcher.id}) Liked ${matched.name} (${matched.id}).`);
+
+ if(isMatched.length){
+     console.log('It\'s a match!');
+ }
+
  return res.json(like);
 
  },
@@ -30,12 +38,12 @@ module.exports = {
 
 },
 
-//DOESNT WORK AT ALL
+
 async getmatchesof(req, res) {
 
  const id = req.params.id;
-
- const usersThatLikedBack = await Like.findAll({ // Gets all users that liked back
+ // Gets all users that liked back
+ const usersThatLikedBack = await Like.findAll({ 
     where: {
         matched_id: id,
         sta_like: ['R', 'S']
@@ -43,18 +51,23 @@ async getmatchesof(req, res) {
     attributes: ['matcher_id'],
 });
 
-
+// Need to transform into vector to pass as argument in next query
+let usersThatLikedBackObj = JSON.stringify(usersThatLikedBack)
+    usersThatLikedBackObj = JSON.parse(usersThatLikedBackObj);
+    usersThatLikedBackObj = usersThatLikedBackObj.map(i => i.matcher_id)
 
  const matches = await Like.findAll({
     where: {
       matcher_id: id,
-      matched_id: usersThatLikedBack,
+      matched_id: usersThatLikedBackObj,
       },
     })
  
- //return res.json(matches);
+return res.json(matches);
 
-}
+},
+
+
 
 
 
